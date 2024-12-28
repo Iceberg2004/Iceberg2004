1e2807ef079f4c6cdccefc4d4f7e9521ad2fb2e3
Iceberg2004 <manglampanse724@gmail.com>
2024-12-28 14:44:54 +0530

    Create TC.java

---
+++ 10.TimeComplexity/TC.java
@@ -0,0 +1,7 @@
+public class TC{
+    public static void main(String[] args) {
+        //1.Always look for worst time complexity
+        //2.Always look at complexity for large size data
+        //3.
+    }
+}
