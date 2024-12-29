22b7a4137f11b67c18eda71da140c8be3aa87c10
Iceberg2004 <manglampanse724@gmail.com>
2024-12-29 14:21:46 +0530

    Update TC.java

--- 10.TimeComplexity/TC.java
+++ 10.TimeComplexity/TC.java
@@ -2,6 +2,57 @@ public class TC{
     public static void main(String[] args) {
         //1.Always look for worst time complexity
         //2.Always look at complexity for large size data
-        //3.
+        //3.ignore all the constants coz, we dont need the correct answer ,we only need the relationship of time  vs input size
+        //4.if complexity is O(n^3 + logN) then ignore 'logN' coz it is less dominating term
+
+        /*
+        TIME COMPLEXITY
+
+          Big O notation
+            It provides the upper bound or worst time complexity
+          
+          Theta 0 notaion
+            it provides the avg time complexity giving the upper as well as lower bound
+
+          omega notation
+            it provides best time complexity or lower bound 
+
+        SPACE COMPLEXITY
+
+            the space taken by the algo + the auxillary space taken by the input data is called time complexity
+
+            There are two types of recursions
+            1.Linear
+            2.Divide and conquer
+
+            TO ACTUALLY FET TEH TIME COMPLEX. OF AN ALOGO THEN USE
+            AKRA BAAZI FORMULA GIVEN BY TWO STUDENS OF MIT
+         
+
+         */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
