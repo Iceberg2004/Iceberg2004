5414b522f5a8e2c739d8eee413a3bb9f7171247d
Iceberg2004 <manglampanse724@gmail.com>
2024-12-30 13:59:04 +0530

    Create BitwiseOp.java

---
+++ 11.BitwiseOpera/BitwiseOp.java
@@ -0,0 +1,49 @@
+public class BitwiseOp {
+    public static void main(String[] args) {
+        /*
+         BITWISE OPERATORS
+         AND
+         OR
+         XOR
+         COMPLEMENT
+
+
+         NUMBER SYSTEMS
+         DECIMAL 1 TO 10
+         BINARY 0 & 1
+         OCTAL 0 TO 7
+         HEXADECIMAL 0 TO F(0 TO 9      &      A TO F)
+
+        CONVERSION:
+        1. decimal to base b
+        keep dividing by base,take remainder unit in opposite
+                        
+                            rem        
+             2|17            
+             2|8             1
+             2|4             0
+             2|2             0
+             2|1             0
+               1
+         ans=(10001)
+        2. base b to decimal
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+         */
+    }
+    
+}
