d4f6a0ed6795191dc406f0c1e22048543f85642e
Iceberg2004 <manglampanse724@gmail.com>
2025-01-09 12:43:42 +0530

    ..

--- 09.Recursion/reverse.java
+++ 09.Recursion/reverse.java
@@ -1,8 +1,12 @@
 public class reverse {
     public static void main(String[] args) {
-     rev1(1743);
-     System.out.println(sum);  //reverse a number using recursion
+    // rev1(1743);
+    // System.out.println(sum);  //reverse a number using recursion
+    int n=1234;
+    System.out.println(rev2(n));
     }
+                 //method 1 to reverse a number
+
     static int sum=0;
     static void rev1(int n){
         if(n==0){
@@ -11,6 +15,25 @@ public class reverse {
         int rem=n%10;
         sum=sum * 10 + rem;
         rev1(n/10);
+    }
+                   //method 2 to reverse a number
+
+    static int rev2(int n){          
+
+    int digits =(int)(Math.log10(n)) +1;
+    return helper(n,digits);
+
+    }
+
+     // making another'helper' function coz we need arguments 
+
+    private static int helper(int n, int digits){
+        if(n%10==n){
+            return n;
+        }
+        int rem= n % 10;
+        return rem * (int)Math.pow(10,digits-1) + helper(n/10,digits-1);
+
     }
     
 }
