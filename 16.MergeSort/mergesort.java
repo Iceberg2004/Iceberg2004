0dca2b3bd14fb9eb5ca82f2c44d453a760a17269
Iceberg2004 <manglampanse724@gmail.com>
2025-02-22 11:00:11 +0530

    ..

--- 16.MergeSort/mergesort.java
+++ 16.MergeSort/mergesort.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-public class mergesort {
+ public class mergesort {
     public static void main(String[] args) {
         int [] arr={5,4,3,2,1};
         arr=merge(arr);
@@ -54,3 +54,5 @@ public class mergesort {
         return mix;
     }
 }
+    
+   
