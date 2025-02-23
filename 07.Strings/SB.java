6af4539cd2053a4f0499fa579965008f6c540ae7
Iceberg2004 <manglampanse724@gmail.com>
2025-02-24 01:37:10 +0530

    jai baba kiiiiii

--- 07.Strings/SB.java
+++ 07.Strings/SB.java
@@ -4,7 +4,7 @@ public class SB {
         String series ="";
         for(int i=0;i<26;i++){
             char ch=(char)('a'+i);
-            series = series + ch +" ";      // its immuatble,every time its create a new object to store the data 
+            series = series + ch +" ";      // string is immuatble,every time its create a new object to store the data 
         }
         System.out.println(series);
 
@@ -13,9 +13,12 @@ public class SB {
         StringBuilder string = new StringBuilder();
         for(int i=0;i<26;i++){
             char ch=(char)('a'+i);
-            string.append(ch+" ");                       //it creates a single object and alter the data in it,it is mutable
+            string.append(ch+" ");                       //string builder creates a single object and alter the data in it,it is mutable
         }
         System.out.println(string.toString());
+
+        // reversing a string
+
         string.reverse();
         System.out.println(string.toString());
     }
---
+++ 18.RecursionQuestion/skipChar.java
@@ -0,0 +1,21 @@
+public class skipChar {
+    public static void main(String[] args) {
+    skip(" ","bcdaaach" );    }
+    static void skip(String p,String up){  // code to remove a from the string or array of characters
+
+        if(up.isEmpty()){
+            System.out.println(p);
+            return ;
+        }
+        char ch = up.charAt(0); //here char at works as indexing in array like : arr[0]
+        if(ch=='a'){
+            skip(p,up.substring(1)); // substring cuts the string and make subset of it from the index we give
+
+        }
+        else{
+            skip(p+ch,up.substring(1));
+
+        }
+    }
+    
+}
